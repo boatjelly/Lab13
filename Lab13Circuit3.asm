337a37231d0d79f806e6e9b2f5f0d755c144c875
Mari <117328477+boatjelly@users.noreply.github.com>
2025-05-09 11:47:34 -0500

    Update Lab13Circuit3.asm
    uses the common cathode display WITHOUT the PCB

--- Lab13Circuit3.asm
+++ Lab13Circuit3.asm
@@ -13,6 +13,8 @@
 ; D6: e on 7 segment display
 ; D7: f on 7 segment display
 ; D8: g on 7 segment display
+; Uses external resistors
+; Uses the configuration from Activity 4
 
 ; any non-repeating tasks should occur outside of the loop
 ; configure D8 as an output pin
@@ -21,52 +23,211 @@ OUT DDRB, r16
 ; configure D2-D7 as output pins
 LDI r16, 0x0FC
 OUT DDRD, r16
+; assign a value of 1111 1100 (FC) to a r1 (0)
+;LDI r27, 0xFC
+
 
 ; loop subroutine starts here
 loop:
-	; start by clearing the output pins
-	; !!!!!!!!!! LEFT OFF HERE
-	CBI PORTB, 0
-	CBI PORTB, 1
-	CBI PORTB, 2
+	call clear_reg
 
-	; input data from the PIND register
-	IN r16, PIND
-	; mask data from the PIND register
-	ANDI r16, 0xE0
+	; This block shows 0
+	; bring in the PORTD!
+	IN r29, PORTD
+	; set bits on PORTD to show 0 on the 7 segment display
+	ORI r29, 0xFC
+	; load it back!
+	OUT PORTD, r29
+	; bring in the PORTB!
+	IN r28, PORTB
+	; set bits on PORTB to show 0 on the 7 segment display
+	ORI r28, 0x00
+	; load it back!
+	OUT PORTB, r28
 
-	; compare to 0x80 (if it is equal to 0x80, then the pushbutton is pressed)
-	CPI r16, 0x80
-	; if r16 = 0x80, turn on the LED
-	BREQ turn_on_rled	; if r16 = 0x80, the code will branch to the turn_on_rled address location
+	call clear_reg
 
-	; compare to 0x40 (if it is equal to 0x40, then the pushbutton is pressed)
-	CPI r16, 0x40
-	; if r16 = 0x40, turn on the LED
-	BREQ turn_on_gled	; if r16 = 0x40, the code will branch to the turn_on_gled address location
+	; This block shows 1
+	; bring in the PORTD!
+	IN r29, PORTD
+	; set bits on PORTD to show 0 on the 7 segment display
+	ORI r29, 0x18
+	; load it back!
+	OUT PORTD, r29
+	; bring in the PORTB!
+	IN r28, PORTB
+	; set bits on PORTB to show 0 on the 7 segment display
+	ORI r28, 0x00
+	; load it back!
+	OUT PORTB, r28
 
-	; compare to 0x20 (if it is equal to 0x20, then the pushbutton is pressed)
-	CPI r16, 0x20
-	; if r16 = 0x20, turn on the LED
-	BREQ turn_on_bled	; if r16 = 0x20, the code will branch to the turn_on_bled address location
+	call clear_reg
 
-	; otherwise, we can jump back to the start of the loop
+	; This block shows 2
+	; bring in the PORTD!
+	IN r29, PORTD
+	; set bits on PORTD to show 0 on the 7 segment display
+	ORI r29, 0x6C
+	; load it back!
+	OUT PORTD, r29
+	; bring in the PORTB!
+	IN r28, PORTB
+	; set bits on PORTB to show 0 on the 7 segment display
+	ORI r28, 0x01
+	; load it back!
+	OUT PORTB, r28
+
+	call clear_reg
+
+	; This block shows 3
+	; bring in the PORTD!
+	IN r29, PORTD
+	; set bits on PORTD to show 0 on the 7 segment display
+	ORI r29, 0x3C
+	; load it back!
+	OUT PORTD, r29
+	; bring in the PORTB!
+	IN r28, PORTB
+	; set bits on PORTB to show 0 on the 7 segment display
+	ORI r28, 0x01
+	; load it back!
+	OUT PORTB, r28
+
+	call clear_reg
+
+	; This block shows 4
+	; bring in the PORTD!
+	IN r29, PORTD
+	; set bits on PORTD to show 0 on the 7 segment display
+	ORI r29, 0x98
+	; load it back!
+	OUT PORTD, r29
+	; bring in the PORTB!
+	IN r28, PORTB
+	; set bits on PORTB to show 0 on the 7 segment display
+	ORI r28, 0x01
+	; load it back!
+	OUT PORTB, r28
+
+	call clear_reg
+
+	; This block shows 5
+	; bring in the PORTD!
+	IN r29, PORTD
+	; set bits on PORTD to show 0 on the 7 segment display
+	ORI r29, 0xB4
+	; load it back!
+	OUT PORTD, r29
+	; bring in the PORTB!
+	IN r28, PORTB
+	; set bits on PORTB to show 0 on the 7 segment display
+	ORI r28, 0x01
+	; load it back!
+	OUT PORTB, r28
+
+	call clear_reg
+
+	; This block shows 6
+	; bring in the PORTD!
+	IN r29, PORTD
+	; set bits on PORTD to show 0 on the 7 segment display
+	ORI r29, 0xF4
+	; load it back!
+	OUT PORTD, r29
+	; bring in the PORTB!
+	IN r28, PORTB
+	; set bits on PORTB to show 0 on the 7 segment display
+	ORI r28, 0x01
+	; load it back!
+	OUT PORTB, r28
+
+	call clear_reg
+
+	; This block shows 7
+	; bring in the PORTD!
+	IN r29, PORTD
+	; set bits on PORTD to show 0 on the 7 segment display
+	ORI r29, 0x9C
+	; load it back!
+	OUT PORTD, r29
+	; bring in the PORTB!
+	IN r28, PORTB
+	; set bits on PORTB to show 0 on the 7 segment display
+	ORI r28, 0x00
+	; load it back!
+	OUT PORTB, r28
+
+	call clear_reg
+
+	; This block shows 8
+	; bring in the PORTD!
+	IN r29, PORTD
+	; set bits on PORTD to show 0 on the 7 segment display
+	ORI r29, 0xFC
+	; load it back!
+	OUT PORTD, r29
+	; bring in the PORTB!
+	IN r28, PORTB
+	; set bits on PORTB to show 0 on the 7 segment display
+	ORI r28, 0x01
+	; load it back!
+	OUT PORTB, r28
+
+	call clear_reg
+
+	; This block shows 9
+	; bring in the PORTD!
+	IN r29, PORTD
+	; set bits on PORTD to show 0 on the 7 segment display
+	ORI r29, 0xBC
+	; load it back!
+	OUT PORTD, r29
+	; bring in the PORTB!
+	IN r28, PORTB
+	; set bits on PORTB to show 0 on the 7 segment display
+	ORI r28, 0x01
+	; load it back!
+	OUT PORTB, r28
+
+	call clear_reg
+
 	JMP loop
 
-turn_on_rled:
-	SBI PORTB, 0
+; This subroutine clears the registers
+clear_reg:
+    ldi  r18, 41
+    ldi  r19, 150
+    ldi  r20, 128
+	; bring in the PORTD!
+	IN r31, PORTD
+	; clear the first 6 bits
+	ANDI r31, 0xFC
+	; load it back!
+	OUT PORTD, r31
+	; bring in the PORTB!
+	IN r30, PORTB
+	; clear just bit 0 on PORTB
+	ANDI r30, 0x01
+	; load it back!
+	OUT PORTB, r30
+	ldi  r18, 41
+    ldi  r19, 150
+    ldi  r20, 128
+	ret ; Return from subroutine 
 
-	; now that the LED is on, go back to the start of the loop
-	JMP loop
 
-turn_on_gled:
-	SBI PORTB, 1
+; Everything from HERE
 
-	; now that the LED is on, go back to the start of the loop
-	JMP loop
+; Assembly code auto-generated
+; by utility from Bret Mulvey
+; Delay 8 000 000 cycles
+; 500ms at 16.0 MHz
 
-turn_on_bled:
-	SBI PORTB, 2
+L1: dec  r20
+    brne L1
+	dec  r19
+    brne L1
+    dec  r18
+    brne L1
 
-	; now that the LED is on, go back to the start of the loop
-	JMP loop
+; to HERE is copy-pasted and NECESSARY
